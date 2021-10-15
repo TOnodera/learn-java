86fc9129fb09ecd8c590dcbd8a3291bb41d0c5d3
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-15 02:23:28 +0000

    別クラスも読み込めるの確認

---
+++ src/lib/Calc.java
@@ -0,0 +1,11 @@
+package lib;
+
+public class Calc {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+}
